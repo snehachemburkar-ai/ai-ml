fe417cc9c97b55a0cd671f07b6f4f3b55a8968ff
snehachemburkar-ai <sneha.chemburkar@gmail.com>
2025-03-09 15:00:00 +0530

    find minimumnumber

---
+++ minNumberInList.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  8 22:36:19 2025
+
+@author: schemburkar
+"""
+import math 
+
+def minArr(a):
+    temp=a[0]
+    for i in range (len(a)-1):
+        if(a[i]<temp):
+            temp=a[i]
+    return temp    
+
+
+
+def findMin(a):
+    a.sort()
+    return(a[0])
+
+
+arr=[77,-25,9,30,14,10,-2]  
+print("Minimum elment in array ",minArr(arr) ) 
+print("Minimum elment in array by sort ",findMin(arr) ) 
